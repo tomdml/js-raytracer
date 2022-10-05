aad8504ac52840efb35b714e76fc96bc854ed0cc
tomdml <tom@tom>
2022-10-05 16:28:35 +0100

    demo up to end of chapter 6

--- main.js
+++ main.js
@@ -6,17 +6,22 @@ const { ray } = require('./src/ray')
 const { Canvas } = require('./src/canvas')
 const { color } = require('./src/color')
 const { sphere } = require('./src/sphere')
+const { material } = require('./src/materials')
+const { point_light } = require('./src/light')
 
 const origin = point(0, 0, -5)
 const wall_z = 10
 const wall_size = 7
-const canvas_pixels = 100
+const canvas_pixels = 500
 const half = wall_size / 2
 const pixel_size = wall_size / canvas_pixels
 
-const canvas = new Canvas(100, 100)
+const canvas = new Canvas(canvas_pixels, canvas_pixels)
 const red = color(1, 0, 0)
 const shape = sphere()
+shape.material = material(color(1, 0.2, 1))
+
+const light = point_light(point(-10, 10, -10), color(1, 1, 1))
 
 for (const y of Array(canvas_pixels).keys()) {
 	const world_y = half - pixel_size * y
@@ -27,7 +32,13 @@ for (const y of Array(canvas_pixels).keys()) {
 
 		const r = ray(origin, position.sub(origin).norm)
 		const xs = shape.intersect(r)
-		if (xs.hit) canvas.write_pixel(x, y, red)
+		if (xs.hit) {
+			const p = r.position(xs.hit.t)
+			const normal = xs.hit.object.normal_at(p)
+			const eye = r.direction.neg()
+			const c = xs.hit.object.material.lighting(light, p, eye, normal)
+			canvas.write_pixel(x, y, c)
+		}
 	}
 
 }
