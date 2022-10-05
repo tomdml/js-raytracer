c3d69a0caab0d80b912ae54d435e31253eaff3ec
tomdml <tom@tom>
2022-10-05 13:47:59 +0100

    implemented hit

--- src/intersection.js
+++ src/intersection.js
@@ -1,9 +1,29 @@
+class Intersections {
+
+	constructor(...args) {
+		this.xs = args
+		this.xs.sort((a, b) => a.t - b.t )
+	}
+
+	get hit() {
+		return this.xs.find(elem => elem.t > 0)
+	}
+
+	get length() {
+		return this.xs.length
+	}
+
+	at(loc) {
+		return this.xs.at(loc)
+	}
+}
+
 function intersection(t, object) {
 	return {t, object}
 }
 
 function intersections(...args) {
-	return [...args]
+	return new Intersections(...args)
 }
 
 module.exports = { intersection, intersections }
--- tests/intersection.test.js
+++ tests/intersection.test.js
@@ -13,8 +13,51 @@ test('aggregating intersections', () => {
 	const s = sphere()
 	const i1 = intersection(1, s)
 	const i2 = intersection(2, s)
+
 	const xs = intersections(i1, i2)
 
 	expect(xs.at(0).t).toBe(1)
 	expect(xs.at(1).t).toBe(2)
+})
+
+test('the hit, when all intersections have positive t', () => {
+	const s = sphere()
+	const i1 = intersection(1, s)
+	const i2 = intersection(2, s)
+
+	const xs = intersections(i1, i2)
+
+	expect(xs.hit).toBe(i1)
+})
+
+test('the hit, when some intersections have negative t', () => {
+	const s = sphere()
+	const i1 = intersection(-1, s)
+	const i2 = intersection(1, s)
+
+	const xs = intersections(i1, i2)
+
+	expect(xs.hit).toBe(i2)
+})
+
+test('the hit, when all intersections have negative t', () => {
+	const s = sphere()
+	const i1 = intersection(-2, s)
+	const i2 = intersection(-1, s)
+
+	const xs = intersections(i1, i2)
+
+	expect(xs.hit).toBe(undefined)
+})
+
+test('the hit is always the lowest nonnegative intersection', () => {
+	const s = sphere()
+	const i1 = intersection(5, s)
+	const i2 = intersection(7, s)
+	const i3 = intersection(-3, s)
+	const i4 = intersection(2, s)
+
+	const xs = intersections(i1, i2, i3, i4)
+
+	expect(xs.hit).toBe(i4)
 })
